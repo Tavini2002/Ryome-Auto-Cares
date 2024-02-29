455905ee37794fae14cad707730c0f5dbbd9d802
Your Name <you@example.com>
2024-02-29 13:07:54 +0530

    add format to code & add my routes (dula)

---
+++ BACKEND/Routes/jeew/suppliers.js
@@ -0,0 +1,92 @@
+const router = require("express").Router();
+let supplier = require("../../Models/supplier");
+
+// CRUD (create page) code segment
+
+router.route("/add").post((req, res) => {
+  const name = req.body.name;
+  const contact = req.body.contact;
+  const address = req.body.address;
+  const country = req.body.country;
+
+  const newSupplier = new supplier({
+    name,
+    contact,
+    address,
+    country,
+  });
+
+  newSupplier
+    .save()
+    .then(() => {
+      res.json("Supplier added");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+// CRUD (all data retreve page) code segment
+router.route("/").get((req, res) => {
+  supplier
+    .find()
+    .then((suppliers) => {
+      res.json(suppliers);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+//CRUD (update page) code segment
+router.route("/update/:id").put(async (req, res) => {
+  let userId = req.params.id; //code for get the individual id from the url
+  const { name, contact, address, country } = req.body;
+
+  //object for supplier update
+  const updateSupplier = {
+    name,
+    contact,
+    address,
+    country,
+  };
+  const update = await supplier
+    .findByIdAndUpdate(userId, updateSupplier)
+    .then(() => {
+      res.status(200).send({ status: "user updated" });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send({ staus: "supplier added can't be done" });
+    });
+});
+//CRUD (delete page) code segment
+
+router.route("/delete/:id").delete(async (req, res) => {
+  let userId = req.params.id;
+  await supplier
+    .findByIdAndDelete(userId)
+    .then(() => {
+      res.status(200).send({ status: "user deleted" });
+    })
+    .catch((err) => {
+      console.log(err.message);
+      res
+        .status(500)
+        .send({ status: "supplier can't be deletes", error: err.message });
+    });
+});
+
+router.route("/get/:id").get(async (req, res) => {
+  let userId = req.params.id;
+  const user = await supplier
+    .findById(userId)
+    .then((supplier) => {
+      res.status(200).send({ status: "user fetched", supplier });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send({ status: "Can't find user", error: err.message });
+    });
+});
+
+module.exports = router;
--- BACKEND/server.js
+++ BACKEND/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const dotenv =require("dotenv");
+const dotenv = require("dotenv");
 const app = express();
 require("dotenv").config();
 
@@ -15,24 +15,51 @@ app.use(bodyParser.json());
 
 // database link variable decleartion
 const URL = process.env.MONGODB_URL;
-
-
 mongoose.connect(URL, {
-//   useCreateIndex: true,
+  //   useCreateIndex: true,
   useNewUrlParser: true,
   useUnifiedTopology: true,
-//   useFindAndModify: false
+  //   useFindAndModify: false
 });
 
-const connection = mongoose.connection; 
-connection.once('open', () => {
+const connection = mongoose.connection;
+connection.once("open", () => {
   console.log("MongoDB Connection Success !");
-})
+});
 
-const supplierRouter =require("./Routes/suppliers.js");
+// JEEW
+const supplierRouter = require("./Routes/jeew/suppliers.js");
+app.use("/supplier", supplierRouter);
 
-app.use("/supplier",supplierRouter);
+// HIMA
 
+
+
+// DULA
+const svcPackagesRouter = require("./Routes/dulanka/svc-packages.js");
+app.use("/svc-packages", svcPackagesRouter);
+
+const svcRecordsRouter = require("./Routes/dulanka/svc-records.js");
+app.use("/svc-records", svcRecordsRouter);
+
+
+// TAVI
+
+
+// CHIYAN
+
+
+// AKIL
+
+
+// SHAJEEH
+
+
+// SANJU
+
+
+
+// server port allocation & server start
 app.listen(PORT, () => {
-  console.log(`Server is up and running at port: ${PORT}`)
-})
+  console.log(`Server is up and running at port: ${PORT}`);
+});
