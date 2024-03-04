97d9630b4fc5c7dc259383a0ca1e633cb6871888
A-Himaa <akilahimaja@hotmail.com>
2024-03-04 15:25:51 +0530

    error fixed

--- FRONTEND/src/App.js
+++ FRONTEND/src/App.js
@@ -11,7 +11,7 @@ import Display from "./Component/Display";
 
 //import Register from "./Component/Register";
 
-import Login from "./Component/Login"; 
+// import Login from "./Component/Login"; 
 // import Update from "./Components/Update";
 // import Test from "./Components/Test";
 import Supplier_home from "./Component/Supplier_home";
@@ -32,9 +32,9 @@ function App() {
           <Route path="/add" element={<Add_Supplier />} />
           <Route path="/display" element={<Display/>}/> 
           <Route path="/delete" element={<Delete/>}/>
-          <Route path="/update" element={<Update/>}/>
+          {/* <Route path="/update" element={<Update/>}/> */}
           <Route path="/booking" element={<BookingPage/>}/>
-          <Route path="/customer" element={<Customerhome/>}/>
+          {/* <Route path="/customer" element={<Customerhome/>}/> */}
          
 
           {/* <Route path="/test" element={<Test/>}/> */}
