af590f06bb3f0a4c37df4c63571c95864fea3ef0
Sivashangar007 <chiyaan0825@gmail.com>
2024-03-20 18:00:58 +0530

    Chiyaan

--- FRONTEND/src/Component/Header.js
+++ FRONTEND/src/Component/Header.js
@@ -1,59 +1,82 @@
 import React from "react";
 import "./Home.css";
 import imgSrc from "./logo.png";
-import "./BookingPage.js"
-import "./BookingPage.css"
+import "./BookingPage.js";
+import "./BookingPage.css";
 import { Link } from "react-router-dom";
 import "./Chome.js";
-import "./Chome.css"
-import "./Register.js"
-import "./Review.js"
-import "./Profile.js"
+import "./Chome.css";
+import "./Register.js";
+import "./Review.js";
+import "./Profile.js";
 
-
-
-function Header(){
-    return(
-       
+function Header() {
+  return (
     <div className="txt">
-        <a href="/"><img src={imgSrc}/></a>
+      <a href="/">
+        <img src={imgSrc} />
+      </a>
       <ul type="none" className="nav_b">
-    <div className="dropdown">
-        <li className="dropdownbtn"><a href="">My function</a></li>
-        <div className="dropdowncontent">
-        <li><a href="/">Home</a></li>
-        <li><a href="/add">Add</a></li>
-        <li><a href="/display">Display</a></li>
-        <li><a href="/update">Update</a></li>
-        <li><a href="/delete">Delete</a></li>
-       
+        <div className="dropdown">
+          <li className="dropdownbtn">
+            <a href="">My function</a>
+          </li>
+          <div className="dropdowncontent">
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/add">Add</a>
+            </li>
+            <li>
+              <a href="/display">Display</a>
+            </li>
+            <li>
+              <a href="/update">Update</a>
+            </li>
+            <li>
+              <a href="/delete">Delete</a>
+            </li>
+          </div>
         </div>
-        </div>
-        <li><a href="/">Home</a></li>
-        <div className="customer-btn">
-            <Link to="/Chome">Customer</Link></div>
-        <li><a href="/add">Supplier</a></li>
-        <li><a href="">Staff</a></li>
-        <li><a href="/">Inventory</a></li>
-        <li><a href="/">Finance</a></li>
-        <li><a href="">Services</a></li>
+
         <li>
-        
-        </li>
-        
-        <li>
-            <div class="book-btn bg-red-500 hover:bg-red-600 text-black hover:text-black rounded-full text-center py-3 px-6">
-                <a href="/booking" class="text-whitesmoke">Book Now</a>
-            </div>
+          <a href="/">Home</a>
         </li>
 
-        
-    </ul>
-    
+        <li>
+          <div className="customer-btn">
+            <a href="/Chome">Customer</a>
+          </div>
+        </li>
 
-</div>
+        <li>
+          <a href="/add">Supplier</a>
+        </li>
+        <li>
+          <a href="">Staff</a>
+        </li>
+        <li>
+          <a href="/">Inventory</a>
+        </li>
+        <li>
+          <a href="/">Finance</a>
+        </li>
+        <li>
+          <a href="">Services</a>
+        </li>
+        <li></li>
 
-    )
+        <li>
+          <div class="book-btn bg-red-500 hover:bg-red-600 text-black hover:text-black rounded-full text-center py-3 px-6">
+            <a href="/booking" class="text-whitesmoke">
+              Book Now
+            </a>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default Header;
--- FRONTEND/src/Component/Register.js
+++ FRONTEND/src/Component/Register.js
@@ -1,64 +1,103 @@
 import React from "react";
 import "./Home.css";
-import "./Register.js"
+import "./Register.js";
+import "./Chome.js";
 
-
-export default function Register(){
-    return(
-        <div class="mx-14 mt-10 border-2 border-blue-400 rounded-lg">
+export default function Register() {
+  return (
+    <div class="mx-14 mt-10 border-2 border-blue-400 rounded-lg">
       <div class="mt-10 text-center font-bold">AUTO FIX</div>
-      <div class="mt-3 text-center text-4xl font-bold">Customer Registration</div>
+      <div class="mt-3 text-center text-4xl font-bold">
+        Customer Registration
+      </div>
       <div class="p-8">
         <div class="flex gap-4">
-          <input type="Name" name="name" class="mt-1 block w-1/2 rounded-md border border-slate-300 bg-white px-3 py-4 placeholder-slate-400 shadow-sm placeholder:font-semibold placeholder:text-gray-500 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 sm:text-sm" placeholder="Full Name *" />
-          <input type="Phone" name="Phone" class="mt-1 block w-1/2 rounded-md border border-slate-300 bg-white px-3 py-4 placeholder-slate-400 shadow-sm placeholder:font-semibold placeholder:text-gray-500 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 sm:text-sm" placeholder="Phone *" />
-          <input type="NIC" name="NIC" class="mt-1 block w-1/2 rounded-md border border-slate-300 bg-white px-3 py-4 placeholder-slate-400 shadow-sm placeholder:font-semibold placeholder:text-gray-500 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 sm:text-sm" placeholder="NIC*" />
-          <input type="email" name="email" class="mt-1 block w-1/2 rounded-md border border-slate-300 bg-white px-3 py-4 placeholder-slate-400 shadow-sm placeholder:font-semibold placeholder:text-gray-500 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 sm:text-sm" placeholder="Email *" />
+          <input
+            type="Name"
+            name="name"
+            class="mt-1 block w-1/2 rounded-md border border-slate-300 bg-white px-3 py-4 placeholder-slate-400 shadow-sm placeholder:font-semibold placeholder:text-gray-500 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 sm:text-sm"
+            placeholder="Full Name *"
+          />
+          <input
+            type="Phone"
+            name="Phone"
+            class="mt-1 block w-1/2 rounded-md border border-slate-300 bg-white px-3 py-4 placeholder-slate-400 shadow-sm placeholder:font-semibold placeholder:text-gray-500 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 sm:text-sm"
+            placeholder="Phone *"
+          />
+          <input
+            type="NIC"
+            name="NIC"
+            class="mt-1 block w-1/2 rounded-md border border-slate-300 bg-white px-3 py-4 placeholder-slate-400 shadow-sm placeholder:font-semibold placeholder:text-gray-500 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 sm:text-sm"
+            placeholder="NIC*"
+          />
+          <input
+            type="email"
+            name="email"
+            class="mt-1 block w-1/2 rounded-md border border-slate-300 bg-white px-3 py-4 placeholder-slate-400 shadow-sm placeholder:font-semibold placeholder:text-gray-500 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 sm:text-sm"
+            placeholder="Email *"
+          />
         </div>
         <div class="flex gap-4">
-          <input type="NUM" name="NUM" class="mt-1 block w-1/2 rounded-md border border-slate-300 bg-white px-3 py-4 placeholder-slate-400 shadow-sm placeholder:font-semibold placeholder:text-gray-500 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 sm:text-sm" placeholder="Vehicle Number *" />
-          <select name="select" id="select" class="block w-1/2 rounded-md border border-slate-300 bg-white px-3 py-4 font-semibold text-gray-500 shadow-sm focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 sm:text-sm">
-            <option  class="font-semibold text-slate-300">Car</option>
-            <option  class="font-semibold text-slate-300">Motor Bike</option>
-            <option  class="font-semibold text-slate-300">Three Wheeler</option>
-            <option  class="font-semibold text-slate-300">Havey Vehicle</option>
-
-
-
+          <input
+            type="NUM"
+            name="NUM"
+            class="mt-1 block w-1/2 rounded-md border border-slate-300 bg-white px-3 py-4 placeholder-slate-400 shadow-sm placeholder:font-semibold placeholder:text-gray-500 focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 sm:text-sm"
+            placeholder="Vehicle Number *"
+          />
+          <select
+            name="select"
+            id="select"
+            class="block w-1/2 rounded-md border border-slate-300 bg-white px-3 py-4 font-semibold text-gray-500 shadow-sm focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 sm:text-sm"
+          >
+            <option class="font-semibold text-slate-300">Car</option>
+            <option class="font-semibold text-slate-300">Motor Bike</option>
+            <option class="font-semibold text-slate-300">Three Wheeler</option>
+            <option class="font-semibold text-slate-300">Havey Vehicle</option>
           </select>
         </div>
         <div class="mt-6">
-                    <label for="password" class="block text-sm font-medium leading-5 text-gray-700">
-                Password
-            </label>
-                    <div class="mt-1 rounded-md shadow-sm">
-                        <input id="password" name="password" type="password" required="" class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5"></input>
-                    </div>
-                </div>
+          <label
+            for="password"
+            class="block text-sm font-medium leading-5 text-gray-700"
+          >
+            Password
+          </label>
+          <div class="mt-1 rounded-md shadow-sm">
+            <input
+              id="password"
+              name="password"
+              type="password"
+              required=""
+              class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5"
+            ></input>
+          </div>
+        </div>
 
-                <div class="mt-6">
-                    <label for="password_confirmation" class="block text-sm font-medium leading-5 text-gray-700">
-                Confirm Password
-            </label>
-                    <div class="mt-1 rounded-md shadow-sm">
-                        <input id="password_confirmation" name="password_confirmation" type="password" required="" class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5"></input>
-                    </div>
-                </div>
-        <dev>
-
-        </dev>
-        
+        <div class="mt-6">
+          <label
+            for="password_confirmation"
+            class="block text-sm font-medium leading-5 text-gray-700"
+          >
+            Confirm Password
+          </label>
+          <div class="mt-1 rounded-md shadow-sm">
+            <input
+              id="password_confirmation"
+              name="password_confirmation"
+              type="password"
+              required=""
+              class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5"
+            ></input>
+          </div>
+        </div>
+        <dev></dev>
       </div>
       <div class="text-center">
-          <a class="text-3xl cursor-pointer rounded-lg bg-blue-900 px-8 py-5 text-sm font-bold text-white mt-10 text-center flex justify-center items-center">Register</a>
-        </div>
-        <b>
-          
-        </b>
+        <a class="text-3xl cursor-pointer rounded-lg bg-blue-900 px-8 py-5 text-sm font-bold text-white mt-10 text-center flex justify-center items-center">
+          Register
+        </a>
+      </div>
+      <b></b>
     </div>
-             
-
-
-        
   );
 }
