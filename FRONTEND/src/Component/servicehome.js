40b54332ab22a8ddbffaffcc3e2b7469d2372648
Your Name <you@example.com>
2024-03-21 20:51:18 +0530

    connect to the home

--- FRONTEND/src/Component/servicehome.js
+++ FRONTEND/src/Component/servicehome.js
@@ -6,7 +6,7 @@ export default function servicehome(){
     return(
         <div className="text-2xl ">
             <ul>
-                <li><a href="/add"><button>Add Service</button></a></li>
+                <li><a href="/Addpkg"><button>Add Service</button></a></li>
                 <li><a href="/display"><button>Display Service</button></a></li>
                 <li><a href="/update"><button>Update Service</button></a></li>
                 <li><a href="/delete"><button>Delete Service</button></a></li>
