e8f08722f77161a47393fd112be36456fbbb8efc
Tavini2002 <taviniperera2002@gmail.com>
2024-03-31 10:02:55 +0530

    booking backend start

--- BACKEND/Models/booking.js
+++ BACKEND/Models/booking.js
@@ -1,16 +1,59 @@
-const mongoose = require('mongoose')
+const mongoose =require('mongoose');
+const { list } = require('tar');
+const Schema = mongoose.Schema;
 
- const newBooking = new mongoose.Schema({
-     fname:String,
-     lname:String,
-     phoneNumber:String,
-     eAddress:String,
-     vType:String,
-     vNum:String,
-     date:Date,
-     time:Date
- });
 
- const Book = mongoose.model('Book', newBooking);
+const bookSchema = new Schema({
+    fname : {
+        type : String,
+        required : true
+    },
+    lname : {
+        type : String,
+        required : true
+    },
+    address : {
+        type : String, 
+        required : true
+    },
+    phoneNum : {
+        type : Number,
+        required : true
+    },
 
-module.exports = Book;
+    eMail : {
+        type : String,
+        required : true
+    },
+
+    vNum : {
+        type : String,
+        required : true
+    },
+
+    vType : {
+        type : String,
+        required : true
+    },
+
+    dDate : {
+        type : Date,
+        required : true
+    },
+
+    tTime : {
+        type : String,
+        required : true
+    },
+
+    serviceBox : {
+        type : String,
+        required : true
+    },
+})
+
+
+const booking = mongoose.model("Booking",bookSchema);
+
+module.exports = booking;
+ 
--- BACKEND/Routes/tavi/booking.js
+++ BACKEND/Routes/tavi/booking.js
@@ -1,16 +1,105 @@
-const mongoose = require('mongoose')
+const router = require("express").Router();
+const booking = require("../../Models/booking");
+let supplier = require("../../Models/supplier");
 
-const newBooking = new mongoose.Schema({
-    fname:String,
-    lname:String,
-    phoneNumber:String,
-    eAddress:String,
-    vType:String,
-    vNum:String,
-    date:Date,
-    time:Date
+// CRUD (create page) code segment
+
+router.route("/bookNow").post((req, res) => {
+  const fname = req.body.fname;
+  const lname = req.body.lname;
+  const address = req.body.address;
+  const phoneNum = req.body.phoneNum;
+  const eMail = req.body.eMail;
+  const vNum = req.body.vNum;
+  const vType = req.body.vType;
+  const dDate = req.body.dDate;
+  const serviceBox = req.body.serviceBox;
+
+  const newBooking = new booking({
+    fname,
+    lname,
+    address,
+    phoneNum,
+    eMail,
+    vNum,
+    vType,
+    dDate,
+    serviceBox,
+  });
+
+  newBooking
+    .save()
+    .then(() => {
+      res.json("Booking Successfull");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
-const Book = mongoose.model('Book', newBooking);
+// CRUD (all data retreve page) code segment
+router.route("/").get((req, res) => {
+  booking
+    .find()
+    .then((booking) => {
+      res.json(booking);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
-module.exports = Book;
+//////
+//CRUD (update page) code segment
+/*router.route("/update/:id").put(async (req, res) => {
+  let userId = req.params.id; //code for get the individual id from the url
+  const { name, contact, address, country } = req.body;
+
+  //object for supplier update
+  const updateSupplier = {
+    name,
+    contact,
+    address,
+    country,
+  };
+  const update = await supplier
+    .findByIdAndUpdate(userId, updateSupplier)
+    .then(() => {
+      res.status(200).send({ status: "user updated" });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send({ staus: "supplier added can't be done" });
+    });
+});
+//CRUD (delete page) code segment
+
+router.route("/delete/:id").delete(async (req, res) => {
+  let userId = req.params.id;
+  await supplier
+    .findByIdAndDelete(userId)
+    .then(() => {
+      res.status(200).send({ status: "user deleted" });
+    })
+    .catch((err) => {
+      console.log(err.message);
+      res
+        .status(500)
+        .send({ status: "supplier can't be deletes", error: err.message });
+    });
+});
+
+router.route("/get/:id").get(async (req, res) => {
+  let userId = req.params.id;
+  const user = await supplier
+    .findById(userId)
+    .then((supplier) => {
+      res.status(200).send({ status: "user fetched", supplier });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send({ status: "Can't find user", error: err.message });
+    });
+}); */
+
+module.exports = router;
